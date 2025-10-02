9c3d5f5a11df042bbc2f10c6f138307670c92e5b
Frederico Ramos <frediramos@gmail.com>
2025-10-02 23:00:10 +0100

    Use Gillian's default memory model as lifting

--- Gillian-JS/lib/Semantics/semantics.ml
+++ Gillian-JS/lib/Semantics/semantics.ml
@@ -1,7 +1,4 @@
-module Legacy_symbolic = LoggingMemory.M
-
-(* module Lifting = Gillian.Symbolic.Legacy_s_memory.Modernize (JSILSMemory.M) *)
-
+module Legacy_symbolic = JSILSMemory.M
 module Symbolic = Gillian.Symbolic.Legacy_s_memory.Modernize (Legacy_symbolic)
 module Concrete = JSILCMemory.M
 module External = External.M
