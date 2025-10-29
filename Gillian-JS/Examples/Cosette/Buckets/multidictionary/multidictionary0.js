2c28a4747576c9b8d9f05a12f415574cd484322a
Frederico Ramos <frediramos@gmail.com>
2025-10-29 08:12:15 +0000

    Make buckets.js dataset consistent with ECMA-SL

--- Gillian-JS/Examples/Cosette/Buckets/multidictionary/multidictionary0.js
+++ Gillian-JS/Examples/Cosette/Buckets/multidictionary/multidictionary0.js
@@ -619,7 +619,7 @@ buckets.MultiDictionary = function (toStrFunction, valuesEqualsFunction) {
         }
         array = parent.get(key);
         if (buckets.isUndefined(array)) {
-          return false;
+            return false;
         }
         if (buckets.arrays.remove(array, value, equalsF)) {
             if (array.length === 0) {
@@ -744,31 +744,3 @@ buckets.MultiDictionary = function (toStrFunction, valuesEqualsFunction) {
 
     return multiDict;
 };
-
-// ---------------------------------- tests -----------------------------------
-
-var dict1 = new buckets.MultiDictionary();
-var dict2 = new buckets.MultiDictionary();
-
-var s1 = symb_string();
-var s2 = symb_string();
-var x1 = symb_number();
-var x2 = symb_number();
-
-Assume(not(x1 = x2));
-
-dict1.set(s1, x1);
-dict1.set(s1, x2);
-
-var res1 = dict1.equals(dict2);
-Assert(not res1);
-
-// BUG: dict.equals takes another dict as input
-// var res2 = dict1.equals(dict1.keys());
-// Assert(not res2);
-
-dict2.set(s2, x1);
-dict2.set(s2, x2);
-
-var res3 = dict1.equals(dict2);
-Assert(((s1 = s2) and res3) or ((not(s1 = s2)) and (not res3)));
--- Gillian-JS/Examples/Cosette/Buckets/multidictionary/multidictionary7.js
+++ Gillian-JS/Examples/Cosette/Buckets/multidictionary/multidictionary7.js
@@ -619,7 +619,7 @@ buckets.MultiDictionary = function (toStrFunction, valuesEqualsFunction) {
         }
         array = parent.get(key);
         if (buckets.isUndefined(array)) {
-          return false;
+            return false;
         }
         if (buckets.arrays.remove(array, value, equalsF)) {
             if (array.length === 0) {
@@ -748,10 +748,10 @@ buckets.MultiDictionary = function (toStrFunction, valuesEqualsFunction) {
 // ---------------------------------- tests -----------------------------------
 
 var dict1 = new buckets.MultiDictionary();
-var dict2 = new buckets.MultiDictionary();
+var dict2;
 
-var s1 = symb_string();
-var s2 = symb_string();
+var s1 = symb_number();
+var s2 = symb_number();
 var x1 = symb_number();
 var x2 = symb_number();
 
@@ -763,12 +763,10 @@ dict1.set(s1, x2);
 var res1 = dict1.equals(dict2);
 Assert(not res1);
 
-// BUG: dict.equals takes another dict as input
-// var res2 = dict1.equals(dict1.keys());
-// Assert(not res2);
+dict2 = new buckets.MultiDictionary();
 
 dict2.set(s2, x1);
 dict2.set(s2, x2);
 
 var res3 = dict1.equals(dict2);
-Assert(((s1 = s2) and res3) or ((not(s1 = s2)) and (not res3)));
+Assert((s1 = s2) and (res3)) or (not(s1 == s2) and (not (res3)));
